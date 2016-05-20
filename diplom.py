f5b07c8e8ee136a0aebd1127e7a464fbda47d9b8
oksanaie <ksnaholovko@gmail.com>
2016-05-20 19:01:21 +0200

    automated push

--- diplom.py
+++ diplom.py
@@ -61,7 +61,7 @@ for line in train:
     else: 
         break
 
-print "Input/Output time: %.3f" % time.time() - start_time
+print "Input/Output time: %.3f" % (time.time() - start_time)
 train_start_time = time.time()
 
 if args.model == "logistic_regression":
@@ -77,7 +77,7 @@ model.fit(train_X, train_y)
 predicted_probability_distribution_test = model.predict_proba(test_X)
 predicted_probability_distribution_train = model.predict_proba(train_X)
 
-print "Training + prediction time: %.3f" % time.time() - train_start_time
+print "Training + prediction time: %.3f" % (time.time() - train_start_time)
 
 MAP_test = mean_average_precision(predicted_probability_distribution_test, 
                                   model.classes_, 
@@ -88,5 +88,5 @@ MAP_train = mean_average_precision(predicted_probability_distribution_train,
                                    train_y) 
 print "MAP on train data: %.3f" % MAP_train
 
-print "Elapsed time: %.3f" % time.time() - start_time
+print "Elapsed time: %.3f" % (time.time() - start_time)
 
